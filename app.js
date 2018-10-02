e0a2dfbca74b3e38664d9947a94c6571e09d5788
Brian Jason <brian@jsdesigns.site>
2018-10-02 14:52:18 -0700

    extracts unique array of categories

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+const fs = require('fs');
 const CSV = require('csv-string')
 
 const filePath = "./tmp/";
@@ -9,10 +9,44 @@ const file = filePath + inputFile;
 
 // working
 
+var contents = fs.readFileSync(file, 'utf8');
 
+// working
 
+const csvArray = CSV.parse(contents);
+const categoryColumHeader = "Primary SIC Code Description";
+const categoryArrayHeaderIndex = HI = 0;
 
+// working
 
+const categoryIndex = csvArray[categoryArrayHeaderIndex]
+                        .indexOf(categoryColumHeader);
+
+// working
+
+var extractCategory = (csvLineIndex, lineItemIndex) => {
+  return csvArray[csvLineIndex][lineItemIndex];
+};
+
+// working
+
+var extractAllCategories = () => {
+  let categoryArray = new Array();
+
+  for(i=0; i<csvArray.length; i++){
+    categoryArray.push(extractCategory(i, categoryIndex));
+  }
+  return categoryArray;
+};
+
+var categoryArray = extractAllCategories();
+
+var uniqueArray = categoryArray.filter(function(elem, pos) {
+                    return categoryArray.indexOf(elem) == pos;
+                  })
+
+// working
+console.log(uniqueArray);
 /*
 
 1. upload .csv file (exported from sales genie)
