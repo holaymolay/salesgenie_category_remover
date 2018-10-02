58988aff7043b6850c5342e77a3af11a95862608
Brian Jason <brian@jsdesigns.site>
2018-10-02 15:18:26 -0700

    moved extract-category logic to separate file

--- app.js
+++ app.js
@@ -1,52 +1,21 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const CSV = require('csv-string')
 
-const filePath = "./tmp/";
-const inputFile = "input.csv";
+const csvCat = require('./lib/category.js');
 
-const file = filePath + inputFile;
+var categoryArray = csvCat.extractAllCategories();
+console.log(categoryArray);
 
-// working
 
-var contents = fs.readFileSync(file, 'utf8');
 
-// working
-
-const csvArray = CSV.parse(contents);
-const categoryColumHeader = "Primary SIC Code Description";
-const categoryArrayHeaderIndex = HI = 0;
-
-// working
-
-const categoryIndex = csvArray[categoryArrayHeaderIndex]
-                        .indexOf(categoryColumHeader);
-
-// working
-
-var extractCategory = (csvLineIndex, lineItemIndex) => {
-  return csvArray[csvLineIndex][lineItemIndex];
-};
-
-// working
-
-var extractAllCategories = () => {
-  let categoryArray = new Array();
-
-  for(i=0; i<csvArray.length; i++){
-    categoryArray.push(extractCategory(i, categoryIndex));
-  }
-  return categoryArray;
-};
-
-var categoryArray = extractAllCategories();
-
+console.log(categoryArray)
 var uniqueArray = categoryArray.filter(function(elem, pos) {
                     return categoryArray.indexOf(elem) == pos;
                   })
 
-// working
 console.log(uniqueArray);
+
+
+
 /*
 
 1. upload .csv file (exported from sales genie)
