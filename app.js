bdf9fdc4e09aa7239986e26f202cb5e1d66edbe1
Brian Jason <brian@jsdesigns.site>
2018-10-02 14:09:48 -0700

    pseudocode

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+
+
+/*
+
+1. upload .csv file (exported from sales genie)
+2. compile array of unique category names from .csv
+3. display list
+  - via html
+  - make items selectable
+  - display sidebar with saved presets
+  - on submit, ask to save category list as a preset
+4. produce csv and display download link
+5. if preset exists & is selected, only display categories that are previously unknown
+  - on submit, ask to update preset
+
+*/
